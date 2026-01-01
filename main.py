f6150f913f63cd876ce392554beb165b789b9cf9
NazarioQp <104754254+NazarioQp@users.noreply.github.com>
2026-01-01 17:40:08 +0300

    main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ if not DATABASE_URL:
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+print("DB URL:", DATABASE_URL)
 
 # ================== MODELS ==================
 
